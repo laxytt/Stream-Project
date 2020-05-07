bdf482bcdaa96765ab0c227c12681a064b9676f4
Wiktor Kukulski <laxytt@gmail.com>
2020-05-07 10:37:12 +0200

    ngrok tunnel

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -28,7 +28,7 @@ class StreamShow extends React.Component {
     const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
-      url: `http://localhost:8000/live/${id}.flv`,
+      url: `http://ecc5d513.ngrok.io/live/${id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
