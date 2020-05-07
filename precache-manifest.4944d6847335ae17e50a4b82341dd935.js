04ee283c524bf7cf15429a7c81408f9204a7b07d
Wiktor Kukulski <laxytt@gmail.com>
2020-05-07 10:42:37 +0200

    Updates

---
+++ precache-manifest.4944d6847335ae17e50a4b82341dd935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1447bbe0b8ee4986561f111fc6abeb",
+    "url": "/Stream-Project/index.html"
+  },
+  {
+    "revision": "9663de44a619b3e62c03",
+    "url": "/Stream-Project/static/js/2.a77eb9f3.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/Stream-Project/static/js/2.a77eb9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3615945264a837ca4b0c",
+    "url": "/Stream-Project/static/js/main.4ed96e56.chunk.js"
+  },
+  {
+    "revision": "62a7071d53b5629c6a0b",
+    "url": "/Stream-Project/static/js/runtime-main.506b00fe.js"
+  }
+]);
